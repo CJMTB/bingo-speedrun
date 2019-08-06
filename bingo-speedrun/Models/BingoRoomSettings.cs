5a9168d44f1f84b7ee2eab4b377305d77a3ba50e
CJMTB <connor.b4898@googlemail.com>
2019-08-06 14:37:59 +0100

    Added BingoRoomSettings.cs

---
+++ bingo-speedrun/Models/BingoRoomSettings.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Threading.Tasks;
+
+namespace BingoSpeedrun.Models
+{
+    [DataContract]
+    public class BingoRoomSettings
+    {
+        [DataMember]
+        public List<string> CardsList = new List<string>();
+
+        public BingoRoomSettings() {
+            // create default cards
+            for (int i = 0; i < 25; i++)
+            {
+                CardsList.Add("Example Bingo Card " + (i + 1));
+            }
+        }
+    }
+}
